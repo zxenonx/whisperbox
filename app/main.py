c1df9c8da5b14006c936fb419938f748bf5f6681
zxenonx <xenon.nemrod@gmail.com>
2026-05-02 14:56:17 +0100

    fix: set allow_credentials=False so CORS_ORIGINS=* is valid

--- app/main.py
+++ app/main.py
@@ -47,7 +47,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins_list,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
